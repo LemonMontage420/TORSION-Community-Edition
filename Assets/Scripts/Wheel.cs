e57dfc251fa83f1a106b5c42600e85ea38529cf0
LemonMontage420 <leythalhendy364@gmail.com>
2025-04-23 14:23:14 -0400

    Added Suspension

---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    public Rigidbody vehicleBody;
+    RaycastHit hit;
+    public LayerMask layerMask;
+    public bool isGrounded;
+
+    public float restLength;
+    public float wheelRadius;
+    float currentLength;
+    float lastLength;
+    public float springStiffness;
+    public float damperStiffness;
+    public Vector3 fZ;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, -transform.up, out hit, restLength + wheelRadius, layerMask)) //Fire a raycast to get the distance between the toplink and the ground
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if(isGrounded) //If we hit something, calculate and apply the generated suspension force
+        {
+            currentLength = hit.distance - wheelRadius;
+            CalculateSuspensionForce();
+            ApplySuspensionForce();
+        }
+        else //If we don't, return the suspension to its resting length (we do not deal with overextended springs)
+        {
+            ResetValues();
+        }
+    }
+
+    void CalculateSuspensionForce()
+    {
+        //Hooke's Law
+        float springDisplacement = restLength - currentLength;
+        float springForce = springDisplacement * springStiffness;
+
+        //Damping Equation
+        float springVelocity = (lastLength - currentLength) / Time.fixedDeltaTime;
+        float damperForce = springVelocity * damperStiffness;
+
+        float suspensionForce = springForce + damperForce;
+        fZ = hit.normal.normalized * suspensionForce; //Multiply by the hit normal to get the direction of the suspension force
+
+        lastLength = currentLength; //Set the lastLength for the next frame
+    }
+    void ApplySuspensionForce()
+    {
+        vehicleBody.AddForceAtPosition(fZ, transform.position); //Apply the suspension force to the vehicle at the toplink position
+    }
+
+    void ResetValues()
+    {
+        lastLength = currentLength = restLength;
+        fZ = Vector3.zero;
+    }
+}
